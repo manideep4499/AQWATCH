d50922b4829d0ffa62ff0c08451194f67c4e24f7
manideep4499 <manideepg892@gmail.com>
2026-02-26 18:10:28 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
 const API_KEY = "237b7e6e406742a2a982207a1d4169ff";
-const BASE_URL = "https://api.themoviedb.org/3";
 const IMG_URL = "https://image.tmdb.org/t/p/w500";
 
-window.onload = () => {
+// Load trending movies when page opens
+window.onload = function () {
     loadTrending();
 };
 
 function loadTrending() {
     fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${API_KEY}`)
-        .then(res => res.json())
-        .then(data => {
-            console.log(data);   // DEBUG
-            displayMovies(data.results);
-        })
-        .catch(err => console.log("Error:", err));
+        .then(response => response.json())
+        .then(data => displayMovies(data.results))
+        .catch(error => console.log("Error:", error));
 }
 
 function searchMovie() {
     const query = document.getElementById("search").value;
 
     fetch(`https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${query}`)
-        .then(res => res.json())
+        .then(response => response.json())
         .then(data => displayMovies(data.results))
-        .catch(err => console.log("Error:", err));
+        .catch(error => console.log("Error:", error));
 }
 
 function displayMovies(movies) {
@@ -33,16 +30,16 @@ function displayMovies(movies) {
 
     movies.forEach(movie => {
         if (movie.poster_path) {
-            const div = document.createElement("div");
-            div.classList.add("movie");
+            const movieDiv = document.createElement("div");
+            movieDiv.classList.add("movie");
 
-            div.innerHTML = `
-                <img src="${IMG_URL}${movie.poster_path}">
+            movieDiv.innerHTML = `
+                <img src="${IMG_URL + movie.poster_path}" />
                 <h3>${movie.title}</h3>
                 <p>⭐ ${movie.vote_average}</p>
             `;
 
-            moviesDiv.appendChild(div);
+            moviesDiv.appendChild(movieDiv);
         }
     });
 }
